b69c68d1aaf2869bf1869656959b96ccc25da5d8
ssumanss <ssuman44@gmail.com>
2019-06-08 16:43:08 +0530

    change made.

--- docs/static/js/app.js
+++ docs/static/js/app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+  navigator.serviceWorker.register('/complex-analysis/sw.js').then(function(registration) {
     console.log('ServiceWorker registration successful!');
   }).catch(function(err) {
     console.log('ServiceWorker registration failed: ', err);
