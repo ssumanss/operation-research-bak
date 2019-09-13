3f381a0b92287294ad414496192082c3199795a3
ssumanss <ssuman44@gmail.com>
2019-09-14 00:27:11 +0530

    change made.

--- docs/utility.py
+++ docs/utility.py
@@ -1,39 +1,21 @@
-def argand(complex_number, vector=False):
-    '''
-    This function takes a complex number.
-    '''
-    import matplotlib.pyplot as plt
-    from numpy import real, imag
-    plt.ylabel('Imaginary')
-    plt.xlabel('Real')
-    plt.axhline(y=0, color='black')
-    plt.axvline(x=0, color='black')
+from fractions import Fraction
+from IPython.display import Markdown, display, HTML
+
+
+def printmd(string):
+    display(Markdown(string))
+
+def printhtml(string):
+    display(HTML(string))
     
-    z = complex_number
-    x = real(z)
-    y = imag(z)
-    plt.plot(x,y, 'bo')
+def fancy_output(res, maximize=False):
+    data = ''
+    for i in res.x:
+        data += str(Fraction(i)) + ','
 
-    if vector: 
-        plt.quiver(0, 0, x, y, angles='xy', scale_units='xy', scale=1, color='g')
-
+    if maximize:
+        optimal = (-1) * res.fun
+    else:
+        optimal = res.fun
         
-def seq(complex_number, n, vector=False):
-    '''
-    This function takes a complex number.
-    '''
-    import matplotlib.pyplot as plt
-    from numpy import real, imag
-    plt.ylabel('Imaginary')
-    plt.xlabel('Real')
-    plt.axhline(y=0, color='black')
-    plt.axvline(x=0, color='black')
-    
-    z = complex_number
-    x = real(z)
-    y = imag(z)
-    plt.plot(x,y, 'bo')
-    plt.text(x, y, str(n), horizontalalignment='right', verticalalignment='bottom')
-
-    if vector: 
-        plt.quiver(0, 0, x, y, angles='xy', scale_units='xy', scale=1, color='g')
+    printmd('The value of optimal is $' + str(Fraction(optimal)) + '$ at ' + '$(' + data[:-1] + ')$')
