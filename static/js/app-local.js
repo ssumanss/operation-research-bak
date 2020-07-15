c8d15857d0b940e9a274d35bedc4d5a631601fbf
ssumanss <ssuman44@gmail.com>
2020-07-15 13:58:00 +0550

    Deployed 4aabc56 with MkDocs version: 1.1

--- static/js/app-local.js
+++ static/js/app-local.js
@@ -5,3 +5,14 @@ if ('serviceWorker' in navigator) {
     console.log('ServiceWorker registration failed: ', err);
   });
 }
+
+MathJax.Hub.Config({
+  extensions: ["tex2jax.js"],
+  jax: ["input/TeX", "output/HTML-CSS"],
+  tex2jax: {
+    inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+    displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+    processEscapes: true
+  },
+  "HTML-CSS": { fonts: ["TeX"] }
+});
